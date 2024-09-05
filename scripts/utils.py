8405c48f384a58f37d4fbdc5739a351e9541e585
Biruk Bizuayehu <brookbizuayehu@gmail.com>
2024-09-05 19:39:20 +0300

    add null value checker function to utils

---
+++ scripts/utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_missing_colum_with_percentage(df):
+    num_missing = df.isnull().sum()
+    num_rows = df.shape[0]
+
+    data = {
+        'num_missing': num_missing, 
+        'percent_missing (%)': [round(x, 2) for x in num_missing / num_rows * 100]
+    }
+
+    stats = pd.DataFrame(data)
+
+    # Filter columns with missing values
+    return stats[stats['num_missing'] != 0]
